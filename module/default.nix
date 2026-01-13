191a63a800926cc3483033155954bb41ef813751
Børge André Jensen <borgeajensen@gmail.com>
2026-01-13 15:41:04 +0100

    feat(extensions): add reapack; feat: add xdg desktop entry (#2)
    * added reapack extension
    
    * add xdg desktop entry

--- module/default.nix
+++ module/default.nix
@@ -62,5 +62,23 @@ in {
 
   config = lib.mkIf cfg.enable {
     home.packages = [ reaper-wrapped ];
+
+    xdg.desktopEntries.reaper = {
+      name = "REAPER";
+      genericName = "Digital Audio Workstation";
+      icon = "cockos-reaper";
+      exec = "reaper %F";
+      type = "Application";
+      categories = ["Audio" "Video" "AudioVideo" "AudioVideoEditing" "Recorder"];
+      mimeType = [
+        "application/x-reaper-project"
+        "application/x-reaper-project-backup"
+        "application/x-reaper-theme"
+      ];
+
+      settings = {
+        StartupWMClass = "REAPER";
+      };
+    };
   };
 }
--- module/extensions/default.nix
+++ module/extensions/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./sws.nix
     ./reaimgui.nix
+    ./reapack.nix
     ./js_reascriptapi.nix
   ];
 }
