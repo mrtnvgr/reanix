476210d307d43e41a16415fa6e2be6d46e00605a
mrtnvgr <martynovegorOF@yandex.ru>
2026-03-14 14:06:37 +0700

    fix(reaper-kb): append new conf to file, reaper should resolve duplicates by itself

--- module/default.nix
+++ module/default.nix
@@ -5,11 +5,6 @@ let
 
   cfg = config.programs.reanix;
 
-  # 1st method of config applying: append lines and remove duplicates
-  # why reaper-kb.ini: properties do not have a value
-  # TODO: auto old script removal???
-  # TODO: auto removal of duplicate keybinds
-
   configApplyingScript = lib.mapAttrsToList (name: value: ''
     ${margesimpson}/bin/margesimpson -t .config/REAPER/${name} ${pkgs.writeText "${name}-patches" value}
   '') (lib.removeAttrs cfg.extraConfig [ "reaper-kb.ini" ]);
@@ -48,8 +43,7 @@ let
   reaper-wrapped = pkgs.writeScriptBin "reaper" /* bash */ ''
     ${lib.concatStringsSep "\n" configApplyingScript}
 
-    # TODO: remove this workaround when TODOs will be implemented
-    cat ${pkgs.writeText "reaper-kb" cfg.extraConfig."reaper-kb.ini"} > ~/.config/REAPER/reaper-kb.ini
+    cat ${pkgs.writeText "reaper-kb" cfg.extraConfig."reaper-kb.ini"} >> ~/.config/REAPER/reaper-kb.ini
 
     ${cfg.hooks.preRun}
 
