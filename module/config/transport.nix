4614839896d3ee0cb835cc0e10c30b0360b35b67
mrtnvgr <martynovegorOF@yandex.ru>
2026-03-15 21:38:45 +0700

    feat(config): add `transport.position` option

--- module/config/transport.nix
+++ module/config/transport.nix
@@ -1,5 +1,8 @@
-{ mkBitfield, mkEnabledOption, ... }:
-{ config, lib, ... }: let
+{ mkBitfield, mkEnabledOption, mkNullyOption, ... }:
+{ inputs, config, pkgs, lib, ... }: let
+  mrtnvgr-lib = inputs.mrtnvgr.lib { inherit pkgs; };
+  inherit (mrtnvgr-lib.strings) unalias;
+
   cfg = config.programs.reanix;
 
   transflags = mkBitfield [
@@ -11,18 +14,35 @@
     false # TODO: "Mousewheel moves transport time selection by beats(alt toggles)"
     false # TODO: "Flash transport yellow on possible audio device underrun"
   ];
+
+  positions = {
+    "Below arrange" = "0";
+    "Above ruler" = "1";
+    "Bottom of main window" = "2";
+    "Top of main window" = "3";
+  };
 in {
   options.programs.reanix.config.transport = {
     playrate.show       =     mkEnabledOption "Show playrate control in transport";
     play_state.show     =     mkEnabledOption "Show play state as text in transport";
     time_signature.show =     mkEnabledOption "Show time signature in transport";
     controls.center     = lib.mkEnableOption  "Center transport controls";
+
+    position = mkNullyOption {
+      type = lib.types.enum (lib.attrNames positions);
+      description = "Transport position";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     programs.reanix.extraConfig."reaper.ini" = /* dosini */ ''
       [reaper]
       transflags=${toString transflags}
+
+      ${lib.optionalString (cfg.config.transport.position != null) ''
+        [reaper]
+        transport_dock_pos=${unalias positions cfg.config.transport.position}
+      ''}
     '';
   };
 }
