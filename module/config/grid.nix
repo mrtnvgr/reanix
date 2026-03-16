7fa119dc288cd011005dfa5ef38c4425617ca431
mrtnvgr <martynovegorOF@yandex.ru>
2026-03-16 11:58:21 +0700

    feat(config): add `grid.z-layer` option

--- module/config/grid.nix
+++ module/config/grid.nix
@@ -1,12 +1,25 @@
 { mkNullyOption, boolToInt, ... }:
-{ config, lib, ... }: let
+{ inputs, config, pkgs, lib, ... }: let
+  mrtnvgr-lib = inputs.mrtnvgr.lib { inherit pkgs; };
+  inherit (mrtnvgr-lib.strings) unalias;
+
   cfg = config.programs.reanix;
   inherit (cfg.config) grid;
+
+  gridinbg2Aliases = {
+    "Over items"    = "0";
+    "Through items" = "1";
+    "Under items"   = "2";
+  };
 in {
   options.programs.reanix.config.grid = {
     dotted = mkNullyOption {
       type = lib.types.bool;
       description = "Show grid lines dotted";
+    };
+
+    z-layer = mkNullyOption {
+      type = lib.types.enum (lib.attrNames gridinbg2Aliases);
     };
   };
 
@@ -15,6 +28,11 @@ in {
       ${lib.optionalString (grid.dotted != null) ''
         [reaper]
         griddot=${boolToInt grid.dotted}
+      ''}
+
+      ${lib.optionalString (grid.z-layer != null) ''
+        [reaper]
+        gridinbg2=${unalias gridinbg2Aliases grid.z-layer}
       ''}
     '';
   };
