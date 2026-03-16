a8d657e535e48c1a15b910a5386d22541f00e0e7
mrtnvgr <martynovegorOF@yandex.ru>
2026-03-16 12:05:31 +0700

    feat(config): add `boot.project` option

--- module/config/boot.nix
+++ module/config/boot.nix
@@ -1,12 +1,28 @@
 { mkNullyOption, boolToInt, ... }:
-{ config, lib, ... }: let
+{ inputs, config, pkgs, lib, ... }: let
+  mrtnvgr-lib = inputs.mrtnvgr.lib { inherit pkgs; };
+  inherit (mrtnvgr-lib.strings) unalias;
+
   cfg = config.programs.reanix;
   boot = cfg.config.boot;
+
+  projectAliases = {
+    "Last active project" = "16";
+    "Last project tabs" = "17";
+    "New project (ignore default template)" = "18";
+    "New project" = "19";
+    "Prompt" = "20";
+  };
 in {
   options.programs.reanix.config.boot = {
     animation = mkNullyOption {
       type = lib.types.bool;
       description = "REAPER's boot animation";
+    };
+
+    project = mkNullyOption {
+      type = lib.types.enum (lib.attrNames projectAliases);
+      description = "What to open at REAPER startup";
     };
   };
 
@@ -16,6 +32,12 @@ in {
         ; Boot animation
         [reaper]
         splashfast=${boolToInt boot.animation}
+      ''}
+
+      ${lib.optionalString (boot.project != null) ''
+        ; Open project at startup: ${boot.project}
+        [reaper]
+        loadlastproj=${unalias projectAliases boot.project}
       ''}
     '';
   };
