3e5de937e92446839604f75440d53409f2077d3a
mrtnvgr <martynovegorOF@yandex.ru>
2026-04-12 14:48:41 +0700

    chore!(config/showpeaks): rename some options

--- module/config/showpeaks.nix
+++ module/config/showpeaks.nix
@@ -3,11 +3,11 @@
   cfg = config.programs.reanix;
 
   showpeaks = mkBitfield [
-    cfg.config.display_peaks_for_media_items
+    cfg.config.items.peaks.enable
     cfg.config.display_peaks_while_recording
     cfg.config.display_peaks_only_for_selected_tracks
     cfg.config.display_peaks_only_for_audible_items
-    cfg.config.draw_faint_peaks_in_folders
+    cfg.config.folders.faint_peaks.enable
     false
     false
     false
@@ -19,11 +19,11 @@
   ];
 in {
   options.programs.reanix.config = {
-    display_peaks_for_media_items          =     mkEnabledOption "Display peaks for media items";
+    items.peaks.enable                     =     mkEnabledOption "Display peaks for media items";
     display_peaks_while_recording          =     mkEnabledOption "Display peaks while recording";
     display_peaks_only_for_selected_tracks = lib.mkEnableOption  "Displau peaks only for selected tracks";
     display_peaks_only_for_audible_items   = lib.mkEnableOption  "Only display peaks for tracks and items that are soloed or not muted";
-    draw_faint_peaks_in_folders            =     mkEnabledOption "Draw faint peaks in folder tracks";
+    folders.faint_peaks.enable             =     mkEnabledOption "Draw faint peaks in folder tracks";
 
     # TODO: peaks display mode
   };
